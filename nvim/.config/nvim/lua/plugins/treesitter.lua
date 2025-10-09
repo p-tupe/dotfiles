b83976a8cbadf66d5919ecd424bb066a3f0312b6
Pritesh Tupe <mail@priteshtupe.com>
2025-10-09 12:32:05 -0400

    Add mdx highlight support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,5 +9,16 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 })
 
+-- Register the mdx filetype
+vim.filetype.add({ extension = { mdx = "mdx" } })
+
+-- Configure treesitter to use the markdown parser for mdx files
+vim.treesitter.language.register("markdown", "mdx")
+
+-- If the current buffer has the extension mdx, but not the newly create filetype, set it
+if vim.endswith(vim.api.nvim_buf_get_name(0), ".mdx") and vim.o.filetype ~= "mdx" then
+	vim.o.filetype = "mdx"
+end
+
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
